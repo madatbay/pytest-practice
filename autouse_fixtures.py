16786a728b8f0a0c455551704d23160e2c9f58c3
Madat Bay <medet.bayramov22@gmail.com>
2021-12-07 20:59:55 +0400

    autouse fixture Ə

---
+++ autouse_fixtures.py
@@ -0,0 +1,26 @@
+from typing import List
+import pytest
+
+
+@pytest.fixture
+def first_entry():
+    return "a"
+
+
+@pytest.fixture
+def order(first_entry):
+    return []
+
+
+@pytest.fixture(autouse=True)
+def append_first(order: List, first_entry):
+    return order.append(first_entry)
+
+
+def test_string_only(order, first_entry):
+    assert order == [first_entry]
+
+
+def test_string_and_int(order, first_entry):
+    order.append(2)
+    assert order == [first_entry, 2]
